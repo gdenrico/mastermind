764b43abb90746fbe0920172a5425da0dcfffa60
gdenrico <enricoboccuni@gmail.com>
2024-12-20 17:45:24 +0000

    Il tuo messaggio di commit

--- contalettere.js
+++ contalettere.js
@@ -4,4 +4,17 @@ function confrontalunghezza () {
 
  let lunghezza1= stringa1.lenght;
  let lunghezza2= stringa2.lenght;
+ let risultato;
+ if (lunghezza1 > lunghezza2){
+     risultato= "la prima stringa è più lunga" + stringa1.lenght;
+     
+
+ } else if (lunghezza2 > lunghezza1) {
+    risultato= "la seconda stringa è più lunga" + stringa2.lenght;
+
+ }
+  else {
+     risultato= "le due stringhe si equivalgono"
+  }
+  document.getElementById("risultato").innerText = risultato;
   }
