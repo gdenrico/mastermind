fed693e741b0cc8f902199764e1b0fdaad9e3007
gdenrico <enricoboccuni@gmail.com>
2024-12-20 17:50:33 +0000

    Il tuo messaggio di commit

--- contalettere.js
+++ contalettere.js
@@ -1,6 +1,6 @@
 function confrontalunghezza () {
- let stringa1 = document.getElementbyid("fnome").value;
- let stringa2 = document.getElementbyid("fnomeb").value;
+ let stringa1 = document.getElementByid("fnome").value;
+ let stringa2 = document.getElementByid("fnomeb").value;
 
  let lunghezza1= stringa1.lenght;
  let lunghezza2= stringa2.lenght;
