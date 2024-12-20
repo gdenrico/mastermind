33549abdf842894cfc1bffdd66c37fb53b1007fe
gdenrico <enricoboccuni@gmail.com>
2024-12-20 18:04:03 +0000

    Il tuo messaggio di commit

--- contalettere.js
+++ contalettere.js
@@ -1,20 +1,22 @@
 function confrontalunghezza () {
- let stringa1 = document.getElementByid("fnome").value;
- let stringa2 = document.getElementByid("fnomeb").value;
+   // Usa getElementById con "I" maiuscola
+   let stringa1 = document.getElementById("fnome").value;
+   let stringa2 = document.getElementById("fnomeb").value;
 
- let lunghezza1= stringa1.lenght;
- let lunghezza2= stringa2.lenght;
- let risultato;
- if (lunghezza1 > lunghezza2){
-     risultato= "la prima stringa è più lunga" + stringa1.lenght;
-     
+   // Correggi "lenght" in "length"
+   let lunghezza1 = stringa1.length;
+   let lunghezza2 = stringa2.length;
+   let risultato;
 
- } else if (lunghezza2 > lunghezza1) {
-    risultato= "la seconda stringa è più lunga" + stringa2.lenght;
+   // Confronta la lunghezza delle stringhe e costruisci il messaggio
+   if (lunghezza1 > lunghezza2) {
+       risultato = "La prima stringa è più lunga di " + lunghezza1 + " caratteri.";
+   } else if (lunghezza2 > lunghezza1) {
+       risultato = "La seconda stringa è più lunga di " + lunghezza2 + " caratteri.";
+   } else {
+       risultato = "Le due stringhe hanno la stessa lunghezza.";
+   }
 
- }
-  else {
-     risultato= "le due stringhe si equivalgono"
-  }
-  document.getElementById("risultato").innerText = risultato;
-  }
+   // Usa getElementById per visualizzare il risultato
+   document.getElementById("risultato").innerText = risultato;
+}
