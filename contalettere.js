ba7ca709b9431d8ed12d79b1c1444d9b6e92c4cf
gdenrico <enricoboccuni@gmail.com>
2024-12-20 17:38:25 +0000

    Il tuo messaggio di commit

---
+++ contalettere.js
@@ -0,0 +1,7 @@
+function confrontalunghezza () {
+ let stringa1 = document.getElementbyid("fnome").value;
+ let stringa2 = document.getElementbyid("fnomeb").value;
+
+ let lunghezza1= stringa1.lenght;
+ let lunghezza2= stringa2.lenght;
+  }
